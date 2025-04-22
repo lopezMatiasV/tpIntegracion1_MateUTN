f7e00b1b0629bdc992e86522821757f0dd4b6d75
Matías Lopez <mts2311@hotmail.com>
2025-04-22 00:16:45 -0300

    first commit, create tp1.py

---
+++ tp1.py
@@ -0,0 +1,3 @@
+""" Conversión de Números:
+Desarrollen un programa que convierta números decimales a binarios y, de forma opcional, también de binario a decimal.
+Extensión: Validar la entrada y mostrar mensajes de error ante datos incorrectos. """
