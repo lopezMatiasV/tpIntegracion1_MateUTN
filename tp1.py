130d68da5c45fe6b13b571c632f950b2fa0b4ada
mati7122 <matias7122@gmail.com>
2025-04-24 14:41:30 -0300

    Comments added

--- tp1.py
+++ tp1.py
@@ -2,7 +2,7 @@
 Desarrollen un programa que convierta números decimales a binarios y, de forma opcional, también de binario a decimal.
 Extensión: Validar la entrada y mostrar mensajes de error ante datos incorrectos. """
 
-
+# Se crea la función Menu para dar opción al usuario del número que desea convertir
 def Menu():
     opt = ''
 
@@ -12,12 +12,12 @@ def Menu():
 
     print("opt ", opt)
 
-    while opt != '1' and opt != '2':
+    while opt != '1' and opt != '2': # Se evalúa que la opción sea válida. Si no es así se continúa pidiendo una opción válida
         opt = input("Por favor escoga una opción válida: ")
 
     return opt
 
-def ConvertirDecimal(numero): # creamos la función que convierte de decimal a binario
+def ConvertirDecimal(numero): # Creamos la función que convierte de decimal a binario
     if numero == 0: 
         return "0" # Si el número ingresado es 0, en binario también es 0
     binario = "" # creamos un string vacío para ir guardando el número en binario
@@ -28,13 +28,18 @@ def ConvertirDecimal(numero): # creamos la función que convierte de decimal a b
     return binario # Fin de la función que convierte de decimal a binario
 
 
-def ConvertirBinario(number): # creamos la función que convierte un número binario a decimal
+def ConvertirBinario(number): # Creamos la función que convierte un número binario a decimal
     sum = 0
-    number = ''.join(reversed(number))
+    reversed = ''
 
-    for i in range(0, len(number)):
-        if number[i] == '1':
-            sum += int(number[i]) * (2 ** i)
+    # Usamos un ciclo for para invertir el orden del string ingresado concatenando el siguiente caratacter con el anterior
+
+    for x in number:
+        reversed = x + reversed
+
+    for i in range(0, len(reversed)): # Se recorre el string anteriormente invertido
+        if reversed[i] == '1': # Se verifica que sólo tomemos en cuenta el digito activo para realizar la ecuación
+            sum += int(reversed[i]) * (2 ** i) # Se realiza la ecuación de suma de productos: base 2 se multiplica por su posición y el producto es multiplicado por un digito activo(1)
 
     return sum
 
@@ -42,10 +47,12 @@ def ConvertirBinario(number): # creamos la función que convierte un número bin
 print("Bienvenido usuario!!")
 print("Software convertidor binario a decimal - decimal a binario")
 
-opt = Menu()
+opt = Menu() # Se llama a la función Menu y su valor se guarda en la variable opt
 
+# Se evalúa la variable opt según los requerimientos del usuario
 if opt == '1':
     print(ConvertirDecimal(int(input("Ingrese un número decimal que quiera convertir en binario: "))))
 elif opt == '2':
     print(ConvertirBinario(input("Ingrese un número binario que quiera convertir en decimal: ")))
+
 
